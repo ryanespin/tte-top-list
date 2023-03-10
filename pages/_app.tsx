5262b444943827b147066021f34ef453c3e2621e
Ryan Espin <ryanespin@gmail.com>
2023-03-10 12:23:28 -0500

    fontawesome 2.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
 import theme from '@/styles'
 import { ChakraProvider } from '@chakra-ui/react'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/pro-solid-svg-icons'
 import type { AppProps } from 'next/app'
 import '@fontsource/krona-one/400.css';
 import '@fontsource/caudex/400.css'
-
-library.add(fas)
 
 function App({ Component, pageProps }: AppProps) {
   return (
