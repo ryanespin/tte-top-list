44d99c6f98898b7eaad861311874bca491bac07a
Ryan Espin <ryanespin@gmail.com>
2024-05-01 14:47:26 -0400

    mo lists.

---
+++ pages/api/listData/top202024/arwenList.ts
@@ -0,0 +1,85 @@
+import { GameList } from "@/components/elements/TopListItem";
+import { gamesList } from "../../gameData";
+
+export const arwenList: GameList = {
+  'Space Base': {
+    ...gamesList['Space Base'],
+    sequence: 1,
+  },
+  'Orléans': {
+    ...gamesList['Orléans'],
+    sequence: 2,
+  },
+  'Obsession': {
+    ...gamesList['Obsession'],
+    sequence: 3,
+  },
+  'The Quacks of Quedlinburg': {
+    ...gamesList['The Quacks of Quedlinburg'],
+    sequence: 4,
+  },
+  'Concordia': {
+    ...gamesList['Concordia'],
+    sequence: 5,
+  },
+  'The Castles of Burgundy': {
+    ...gamesList['The Castles of Burgundy'],
+    sequence: 6,
+  },
+  'Twilight Imperium: Fourth Edition': {
+    ...gamesList['Twilight Imperium: Fourth Edition'],
+    sequence: 7,
+  },
+  'Too Many Bones': {
+    ...gamesList['Too Many Bones'],
+    sequence: 8,
+  },
+  'Cartographers': {
+    ...gamesList['Cartographers'],
+    sequence: 9,
+  },
+  'Blood on the Clocktower': {
+    ...gamesList['Blood on the Clocktower'],
+    sequence: 10,
+  },
+  'Alchemists': {
+    ...gamesList['Alchemists'],
+    sequence: 11,
+  },
+  'Russian Railroads': {
+    ...gamesList['Russian Railroads'],
+    sequence: 12,
+  },
+  'Kanban EV': {
+    ...gamesList['Kanban EV'],
+    sequence: 13,
+  },
+  'Rococo: Deluxe Edition': {
+    ...gamesList['Rococo: Deluxe Edition'],
+    sequence: 14,
+  },
+  'Food Chain Magnate': {
+    ...gamesList['Food Chain Magnate'],
+    sequence: 15,
+  },
+  'Star Wars Rebellion': {
+    ...gamesList['Star Wars Rebellion'],
+    sequence: 16,
+  },
+  'Brass: Birmingham': {
+    ...gamesList['Brass: Birmingham'],
+    sequence: 17,
+  },
+  'Planet Unknown': {
+    ...gamesList['Planet Unknown'],
+    sequence: 18,
+  },
+  'A Feast for Odin': {
+    ...gamesList['A Feast for Odin'],
+    sequence: 19,
+  },
+  'Everdell': {
+    ...gamesList['Everdell'],
+    sequence: 20,
+  },
+}
