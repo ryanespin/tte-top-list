3f85c71e29bd583f65ddaad91dd9987b9f64b739
Ryan Espin <ryanespin@gmail.com>
2025-04-01 12:00:48 -0400

    callers top 20.

--- pages/api/listData/top202025/callersList.ts
+++ pages/api/listData/top202025/callersList.ts
@@ -2,147 +2,124 @@ import { GameList } from "@/components/elements/TopListItem";
 import { gamesList } from "../../gameData";
 
 export const callersList: GameList = {
+  'Ark Nova': {
+    ...gamesList['Ark Nova'],
+    badgeText: '#6 • 2024',
+    sequence: 1,
+    title: "munkishine's #2"
+  },
+  'Wingspan': {
+    ...gamesList['Wingspan'],
+    badgeText: 'New to List',
+    sequence: 2,
+    title: "RachelDav's #2"
+  },
   'Lost Ruins of Arnak': {
     ...gamesList['Lost Ruins of Arnak'],
-    sequence: 1,
-    title: "ericbuscemi's #9"
+    badgeText: '#1 • 2024',
+    sequence: 3,
+    title: "DNix7's #9"
+  },
+  'Azul': {
+    ...gamesList['Azul'],
+    badgeText: 'New to List',
+    sequence: 4,
+    title: "bmalkin1's #18"
+  },
+  'The Castles of Burgundy': {
+    ...gamesList['The Castles of Burgundy'],
+    badgeText: 'New to List',
+    sequence: 5,
+    title: "Buckaroo's #2"
+  },
+  'Heat: Pedal to the Metal': {
+    ...gamesList['Heat: Pedal to the Metal'],
+    badgeText: 'New to List',
+    sequence: 6,
+    title: "RMCSquirrel's #13"
   },
   'Dune: Imperium': {
     ...gamesList['Dune: Imperium'],
-    sequence: 2,
-    title: "DNix7's #2"
-  },
-  'The Quacks of Quedlinburg': {
-    ...gamesList['The Quacks of Quedlinburg'],
-    crossover: {
-      Chris: 3,
-      Anthony: 4,
-      Arwen: 4
-    },
-    sequence: 3,
-    title: "drjrpgfan's #5"
-  },
-  'Brass: Birmingham': {
-    ...gamesList['Brass: Birmingham'],
-    crossover: {
-      'Ryan': 3,
-      'Mista Rau': 5,
-      Arwen: 17
-    },
-    sequence: 4,
-    title: "RMCSquirrel's #4"
-  },
-  'Pax Pamir: Second Edition': {
-    ...gamesList['Pax Pamir: Second Edition'],
-    sequence: 5,
-    title: "XoeAllred's #11"
-  },
-  'Ark Nova': {
-    ...gamesList['Ark Nova'],
-    sequence: 6,
-    title: "tlance8's #1"
-  },
-  'Age of Steam': {
-    ...gamesList['Age of Steam'],
-    crossover: {
-      Ryan: 10
-    },
+    badgeText: '#2 • 2024',
     sequence: 7,
-    title: "Nick.booth's #6"
+    title: "The BoardGame Sherpa's #4"
   },
   'Cthulhu: Death May Die': {
     ...gamesList['Cthulhu: Death May Die'],
+    badgeText: '#8 • 2024',
     sequence: 8,
-    title: "FFAdventureCo's #2"
-  },
-  'Food Chain Magnate': {
-    ...gamesList['Food Chain Magnate'],
-    badgeText: '#20 • 2023',
-    crossover: {
-      Ryan: 1,
-      Arwen: 15
-    },
-    sequence: 9,
-    title: "MeepleandtheMoose's #1"
-  },
-  'Arkham Horror: The Card Game': {
-    ...gamesList['Arkham Horror: The Card Game'],
-    crossover: {
-      'Mista Rau': 9
-    },
-    sequence: 10,
-    title: "Munkishine's #1"
-  },
-  'Cascadia': {
-    ...gamesList['Cascadia'],
-    crossover: {
-      Anthony: 1,
-      Brian: 7,
-      "Chris": 4
-    },
-    sequence: 11,
-    title: "ChewieHuey's #2"
-  },
-  'Targi': {
-    ...gamesList['Targi'],
-    badgeText: '#1 • 2023',
-    crossover: {
-      Chris: 1,
-      "Mista Rau": 2
-    },
-    sequence: 12,
-    title: "Celmario's #1"
-  },
-  'Too Many Bones': {
-    ...gamesList['Too Many Bones'],
-    crossover: {
-      Arwen: 8
-    },
-    sequence: 13,
-    title: "Gabe's #9"
+    title: "drjrpgfan's #5"
   },
   'Battlestar Galactica': {
     ...gamesList['Battlestar Galactica'],
-    crossover: {
-      Chris: 7
-    },
+    badgeText: '#14 • 2024',
+    sequence: 9,
+    title: "Ecto_Fox's #1"
+  },
+  'Dice Throne': {
+    ...gamesList['Dice Throne'],
+    badgeText: 'New to List',
+    sequence: 10,
+    title: "Ongo_gablogian_'s #9"
+  },
+  'Cascadia': {
+    ...gamesList['Cascadia'],
+    badgeText: '#11 • 2024',
+    sequence: 11,
+    title: "The D.J. Dan's #6"
+  },
+  'Marvel Champions: The Card Game': {
+    ...gamesList['Marvel Champions: The Card Game'],
+    badgeText: 'New to List',
+    sequence: 12,
+    title: "Epigone's #4"
+  },
+  'Ticket to Ride': {
+    ...gamesList['Ticket to Ride: Europe'],
+    badgeText: 'New to List',
+    sequence: 13,
+    title: "bobafterdark's #10"
+  },
+  'Age of Steam': {
+    ...gamesList['Age of Steam'],
+    badgeText: '#7 • 2024',
     sequence: 14,
-    title: "indianajo883's #7"
+    title: "tlance8's #3"
   },
-  'Burgle Bros Series': {
-    ...gamesList['Burgle Bros.'],
+  'Star Wars: The Deckbuilding Game': {
+    ...gamesList['Star Wars: The Deckbuilding Game'],
+    badgeText: 'New to List',
     sequence: 15,
-    title: "phillyvenkman's #3"
+    title: "Brian Chandler's #1"
   },
-  'Crokinole': {
-    ...gamesList['Crokinole'],
-    crossover: {
-      Ryan: 4,
-    },
+  'Knarr': {
+    ...gamesList['Knarr'],
+    badgeText: 'New to List',
     sequence: 16,
-    title: "Peloquin17's #5"
+    title: "computerblue16's #14"
   },
-  'Bärenpark': {
-    ...gamesList['Bärenpark'],
+  'Spirit Island': {
+    ...gamesList['Spirit Island'],
+    badgeText: 'New to List',
     sequence: 17,
-    title: "Kristen (bga Bruinsbuzz)'s #2"
+    title: "Skye Larsen's #1"
   },
-  'Just One': {
-    ...gamesList['Just One'],
+  'Brass: Birmingham': {
+    ...gamesList['Brass: Birmingham'],
+    badgeText: '#4 • 2024',
     sequence: 18,
-    title: "RachelDav's #1"
+    title: "peloquin17's #10"
   },
-  'Ashes Reborn: Rise of the Phoenixborn': {
-    ...gamesList['Ashes Reborn: Rise of the Phoenixborn'],
+  'Flip 7': {
+    ...gamesList['Flip 7'],
+    badgeText: 'New to List',
     sequence: 19,
-    title: "Oxirador's #1"
+    title: "bruinsbuzz's #5"
   },
-  'Dominion': {
-    ...gamesList['Dominion'],
-    crossover: {
-      Ryan: 13
-    },
+  'Carcassonne': {
+    ...gamesList['Carcassonne'],
+    badgeText: 'New to List',
     sequence: 20,
-    title: "Sarah Reed's #2"
+    title: "aerials' #2"
   },
 }
